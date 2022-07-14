526a1d8830b5748497bf57c2bcbc855d3403d309
Leonardo Raposo <apoena@live.com>
2022-07-14 11:01:00 -0300

    Calculo de area

---
+++ desafio011.py
@@ -0,0 +1,5 @@
+a = float(input('Digite a altura da parede: '))
+l = float(input('Digite a largura da parede: '))
+m2 = a * l
+t = m2 / 2
+print(f'Equivale a {m2} M² e serão necessários {t} L de tinta para pintar essa área')
