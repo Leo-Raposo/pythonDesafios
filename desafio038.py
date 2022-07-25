971d1f48637ccf8b95b62624a4da3c5fc7df9e20
Leonardo Raposo <apoena@live.com>
2022-07-25 16:31:22 -0300

    Comparando bases numéricas!

---
+++ desafio038.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite o 1º número inteiro: '))
+n2 = int(input('Digite o 2º número inteiro: '))
+if n1 > n2:
+    print(f'O número {n1} é maior!')
+elif n2 > n1:
+    print(f'O número {n2} é maior!')
+else:
+    print('Os números são iguais!')
