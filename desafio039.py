d7f1cbe6b7b019a40bcaa67d1bdcc9b28ed25d5a
Leonardo Raposo <apoena@live.com>
2022-07-26 16:05:21 -0300

    Alistamento Militar v2.0

--- desafio039.py
+++ desafio039.py
@@ -1,12 +1,19 @@
+from datetime import date
 ano = int(input('Digite o ano do seu nascimento: '))
-
-idade = 2022 - ano
-a = idade - 18
-b = 18 - idade
-
-if idade < 18:
-    print(f'Você tem \33[1:30:42m{idade} anos!\33[m Ainda não está na hora de se alistar, faltam {b} anos!')
+data = date.today().year
+idade = data - ano
+if ano > data:
+    print('Por favor insira uma data válida!')
+elif idade < 18:
+    saldo = 18 - idade
+    print(f'Você tem \33[1:30:42m{idade} anos!\33[m Ainda não está na hora de se alistar, faltam {saldo} anos!')
+    a = data + saldo
+    print(f'Seu alistamento será em {a}')
 elif idade == 18:
     print(f'Você tem \33[1:30:42m{idade} anos!\33[m Está na hora de se alistar!')
-else:
-    print(f'Você tem \33[1:30:42m{idade} anos!\33[m  Se passou {a} anos do alistamento, procure o serviço Militar para mais informações!!!')
+elif idade > 18:
+    saldo = idade - 18
+    print(f'Você tem \33[1:30:42m{idade} anos!\33[m  Se passaram {saldo} anos do alistamento!')
+    a = data - saldo
+    print(f'Seus alistamento foi em {a}')
+
