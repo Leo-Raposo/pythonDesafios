22472a49bd6e31d3e1c81185d72a39d149a9a55d
Leonardo Raposo <apoena@live.com>
2022-07-26 09:38:38 -0300

    Analisador de triângulos v2.0

---
+++ desafio042.py
@@ -0,0 +1,16 @@
+print('=-=' * 20)
+print('Analisador de Triângulos')
+print('=-=' * 20)
+r1 = float(input('Digite o comprimeto da reta 1: '))
+r2 = float(input('Digite o comprimeito da reta 2: '))
+r3 = float(input('Digite o comprimeito da reta 3: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os seguimentos acima PODEM FORMAR um triângulo ', end='')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO!')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO!')
+    else:
+        print('ISOCELES')
+else:
+    print('Os seguimentos acima NÃO PODEM FORMAR um triângulo')
