1f7e57598457c0151ac4d603c5025e12c8502f1a
Leonardo Raposo <apoena@live.com>
2022-07-26 14:11:02 -0300

    Calculo de Empréstimo! v2.0

--- desafio036.py
+++ desafio036.py
@@ -3,13 +3,15 @@ print('\33[1:33:40m =-=\033[m' * 20)
 print('\33[1:33:40m SISTEMA BANCÁRIO\033[m')
 print('\33[1:33:40m =-=\033[m' * 20)
 
-imovel = float(input('Digite o valor do imóvel: '))
-salario = float(input('Digite o valor do salário: '))
-ano = float(input('Em quantos meses deseja pagar o imóvel?'))
-em = imovel / ano
+imovel = float(input('Digite o valor do imóvel: R$ '))
+salario = float(input('Digite o valor do salário: R$ '))
+ano = int(input('Em quantos anos deseja pagar o imóvel? '))
+em = imovel / (ano * 12)
 print('PROCESSANDO...')
-sleep(2)
+sleep(1)
 if em > salario * 0.3:
-    print(f'\33[1:30:41m Infelizmente\33[m seu emprestimo não foi aprovado!')
+    print(f"""Seu emprestimo de R$ {imovel:.2f} em {ano} anos tem pestações de R$ {em:.2f}!
+Emprestimo \33[1:30:41mNEGADO\33[m""")
 else:
-    print(f'\33[1:30:42m Parabés!\033[m Seu emprestimo de R$ {imovel:.2f} foi aprovado, e terá parcelas de R$ {em:.2f}!')
+    print(f"""Seu emprestimo de R$ {imovel:.2f} em {ano} anos tem prestações de R$ {em:.2f}!
+Emprestimo \33[1:30:42mAPROVADO!\033[m""")
