1ed7a42b1fb6f1730164220a6f8b0e2fc250cd9a
Leonardo Raposo <apoena@live.com>
2022-07-26 18:53:15 -0300

    Jokenpô v2.0

--- desafio045.py
+++ desafio045.py
@@ -1,26 +1,46 @@
 from random import randint
 from time import sleep
 print('VAMOS JOGAR JOKENPÔ? FAÇA SUA ESCOLHA!!!')
-print("""[ 1 ] Pedra
-[ 2 ] Papel
-[ 3 ] Tesoura""")
+itens = ('PEDRA', 'PAPEL', 'TESOURA')
+pc = randint(0, 2)
+print("""[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura""")
 eu = int(input('Selecione a opção: '))
-pc = randint(1, 3)
-print('PROCESSANDO...')
+print('\033[1:31:40mJO\033[m')
 sleep(1)
-if eu == 1 and pc == 2:
-    print('Você escolheu PEDRA e eu PAPEL. Eu ganhei!')
-elif eu == 1 and pc == 3:
-    print('Você escolheu PEDRA e eu TESOURA!. Parabéns, você ganhou!')
-elif eu == 2 and pc == 1:
-    print('Você escolheu PAPEL e eu PEDRA! Parabéns, você ganhou!')
-elif eu == 2 and pc == 3:
-    print('Você escolheu PAPEL e eu TESOURA! Eu ganhei!')
-elif eu == 3 and pc == 1:
-    print('Você escolheu TESOURA e eu PEDRA! Eu ganhei!')
-elif eu == 3 and pc == 2:
-    print('Você escolheu TESOURA e eu PAPEL! Parabéns, você ganhou!')
-elif eu > 3:
-    print('Número não encontrado! Escolha um número acima!')
-else:
-    print('Deu empate!')
+print('\033[1:33:40mKEN\33[m')
+sleep(1)
+print('\033[1:35:40mPÔ!!!\033[m')
+sleep(0.5)
+print('*~' * 12)
+print(f'Computador jogou {itens[pc]}')
+print(f'Jogador jogou {itens[eu]}')
+print('*~' * 12)
+if pc == 0: #PC jogou PEDRA
+    if eu == 0:
+        print('\033[1:30:43mEMPATE\033[m')
+    elif eu == 1:
+        print('\033[1:30:42mJOGADOR VENCE\033[m')
+    elif eu == 2:
+        print('\033[1:30:41mCOMPUTADOR VENCE\033[m')
+    else:
+        print('\033[1:31:40mJOGADA INVÁLIDA\033[m')
+elif pc == 1: #PC jogou PAPEL
+    if eu == 0:
+        print('\033[1:30:41mCOMPUTADOR VENCE\033[m')
+    elif eu == 1:
+        print('\033[1:30:43mEMPATE\033[m')
+    elif eu == 2:
+        print('\033[1:30:42mJOGADOR VENCE\033[m')
+    else:
+        print('\033[1:31:40mJOGADA INVÁLIDA\033[m')
+elif pc == 2: #PC jogou TESOURA
+    if eu == 0:
+        print('\033[1:30:42mO JOGADOR VENCE\033[m')
+    elif eu == 1:
+        print('\033[1:30:41mCOMPUTADOR VENCE\033[m')
+    elif eu == 2:
+        print('\033[1:30:43mEMPATE\033[m')
+    else:
+        print('\033[1:31:40mJOGADA INVÁLIDA\033[m')
